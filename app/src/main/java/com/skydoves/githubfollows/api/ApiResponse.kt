0ef1ee78b7517c74aac0cc180f57ea4d6bedca8f
skydoves <skydoves@naver.com>
2018-02-20 00:45:25 +0900

    to kotlin

---
+++ app/src/main/java/com/skydoves/githubfollows/api/ApiResponse.kt
@@ -0,0 +1,94 @@
+package com.skydoves.githubfollows.api
+
+import android.support.v4.util.ArrayMap
+import com.google.gson.Gson
+import com.skydoves.githubfollows.models.Envelope
+import retrofit2.Response
+import timber.log.Timber
+import java.io.IOException
+import java.util.regex.Pattern
+
+/**
+ * Developed by skydoves on 2018-02-20.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+
+class ApiResponse<T> {
+    val code: Int
+    val body: T?
+    private var links: MutableMap<String, String>
+    private val gson: Gson
+    val envelope: Envelope?
+
+    val isSuccessful: Boolean
+        get() = code in 200..300
+
+    val nextPage: Int?
+        get() {
+            val next = links[NEXT_LINK] ?: return null
+            val matcher = PAGE_PATTERN.matcher(next)
+            if (!matcher.find() || matcher.groupCount() != 1) {
+                return null
+            }
+            try {
+                return Integer.parseInt(matcher.group(1))
+            } catch (ex: NumberFormatException) {
+                Timber.w("cannot parse next page from %s", next)
+                return null
+            }
+        }
+
+    init {
+        gson = Gson()
+        links = emptyMap<String, String>().toMutableMap()
+    }
+
+    constructor(error: Throwable) {
+        code = 500
+        body = null
+        envelope = null
+    }
+
+    constructor(response: Response<T>) {
+        code = response.code()
+        if (response.isSuccessful) {
+            body = response.body()
+            envelope = null
+        } else {
+            var message: String? = null
+            response.errorBody()?.let {
+                try {
+                    message = response.errorBody()!!.string()
+                } catch (ignored: IOException) {
+                    Timber.e(ignored, "error while parsing response")
+                }
+            }
+
+            message?.apply {
+                if(isNullOrEmpty() || trim { it <= ' ' }.isEmpty()) {
+                    message = response.message()
+                }
+            }
+
+            envelope = gson.fromJson(message, Envelope::class.java)
+            body = null
+        }
+
+        val linkHeader = response.headers().get("link")
+        linkHeader?.let {
+            links = ArrayMap()
+            val matcher = LINK_PATTERN.matcher(linkHeader)
+            while (matcher.find()) {
+                if (matcher.groupCount() == 2) {
+                    links.put(matcher.group(2), matcher.group(1))
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val LINK_PATTERN = Pattern.compile("<([^>]*)>[\\s]*;[\\s]*rel=\"([a-zA-Z0-9]+)\"")
+        private val PAGE_PATTERN = Pattern.compile("\\bpage=(\\d+)")
+        private val NEXT_LINK = "next"
+    }
+}
---
+++ app/src/main/java/com/skydoves/githubfollows/di/ActivityModule.kt
@@ -0,0 +1,25 @@
+package com.skydoves.githubfollows.di
+
+import com.skydoves.githubfollows.view.ui.detail.DetailActivity
+import com.skydoves.githubfollows.view.ui.main.MainActivity
+import com.skydoves.githubfollows.view.ui.search.SearchActivity
+
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+/**
+ * Developed by skydoves on 2018-01-20.
+ * Copyright (c) 2018 skydoves rights reserved.
+ */
+
+@Module
+abstract class ActivityModule {
+    @ContributesAndroidInjector
+    internal abstract fun contributeMainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeSearchActivity(): SearchActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeDetailActivity(): DetailActivity
+}
