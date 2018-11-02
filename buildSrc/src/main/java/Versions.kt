8408975eaae193d0833ad0ea55203de84057aafb
skydoves <skydoves@naver.com>
2018-11-02 23:01:44 +0900

    updated gradle & support library & kotlin version, added AndroidVeil

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -8,16 +8,16 @@ object Versions {
     private const val versionMinor = 0
     private const val versionPatch = 0
 
-    const val compileSdk = 27
+    const val compileSdk = 28
     const val minSdk = 16
-    const val targetSdk = 27
+    const val targetSdk = 28
     const val versionCode = versionMajor * 100 + versionMinor * 10 + versionPatch
     const val versionName = "$versionMajor.$versionMinor.$versionPatch"
 
-    const val gradle = "3.1.3"
+    const val gradle = "3.2.0"
     const val junit = "4.12"
-    const val supportLibrary = "27.1.1"
-    const val kotlin = "1.2.50"
+    const val supportLibrary = "28.0.0"
+    const val kotlin = "1.2.70"
     const val anko = "0.10.1"
     const val archComponent = "1.1.1"
     const val archCoreTesting = "1.1.0"
@@ -32,7 +32,8 @@ object Versions {
     const val glide = "4.5.0"
     const val powerMenu = "1.0.9"
     const val androidSvg = "1.2.1"
-    const val shimmer = "1.1.0"
+    const val baseAdapter = "0.1.1"
+    const val androidVeil = "1.0.1"
     const val mockito = "2.7.22"
     const val mockWebServer = "3.9.0"
     const val stetho = "1.5.0"
